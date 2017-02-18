7cfdceb4610d4e0f1e54ec2cc83f6e11c4eb22f0
Tajul2810 <bytajul2810.com>
2017-02-18 12:26:13 +0100

    Cuenta general

--- count.c
+++ count.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <readfile.h>
 
-int contar(Pizza pizza){
-    int i=0, j=0;
+int contar(Pizza pizza, x1, y1, x2, y2){
+
+  if(!pizza)
+    return -3;
+  if(x1>=x2 || x1>=pizza->R || x2>=pizza->R)
+    return -1;
+  if(y1>=y2 || y1>=pizza->C || y2>=pizza->C)
+    return -2;
+
+    int i, j;
     int count;
     char c;
 
     count=0;
 
-    for (i=0; i<pizza.R; i++){
-        for(j=0; j<pizza.C; j++){
-            c = pizza.cells[i][j];
+    for (i=x; i<x2; i++){
+        for(j=y; j<y2; j++){
+            c = pizza->cells[i][j];
             if(c == 'T')
               count++;
             }
