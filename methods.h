1288f2a0bc8269fdf2788b9c8a9b7bcb8cf96858
RodrigoDePool <rodrigodp05@gmail.com>
2017-02-18 13:05:43 +0100

    interfaz de solution

--- methods.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct{
-    int nslices;
-    int *sol[4];
-} Solution;
-
---
+++ solution.h
@@ -0,0 +1,44 @@
+#ifndef SOLUTION_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SLICES 3000
+
+typedef struct _Solution Solution;
+typedef struct _Slice Slice;
+
+typedef struct{
+    int nslices;
+    Slice slices[MAX_SLICES];  
+} Solution;
+
+
+struct{
+    int sol[4];
+} Slice;
+
+
+Solution *newSolution();
+
+int addSlice(Solution *s,Slice *sl);
+int printSolution(Solution *s, char *path);
+
+
+Slice *newSlice(int ri, int ci, int rf, int cf);
+int getIRow(Slice *sl);
+int getFRow(Slice *sl);
+int getICol(Slice *sl);
+int getFCol(Slice *sl);
+
+int newFCol(Slice *sl, int cf);
+int newFRow(Slice *sl, int rf);
+int newICol(Slice *sl, int ci);
+int newIRow(Slice *sl, int ri);
+
+
+
+void freeSlice(Slice *sl);
+void freeSolution(Solution *s);
+
+
+#endif
