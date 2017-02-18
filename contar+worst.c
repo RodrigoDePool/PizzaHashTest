27e5389dff942e1796cbd86096e22188769c9f46
Tajul2810 <bytajul2810.com>
2017-02-18 11:51:03 +0100

    Contar, voy a hacer worst fit

---
+++ contar+worst.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readfile.h>
+
+int* contar(Pizza pizza){
+    int i=0, j=0;
+    int *count;
+    char c;
+
+    count = (int *)malloc(2*sizeof(int));
+    if(!count)
+      return NULL;
+    count[0]=count[1]=0;
+
+
+    for (i=0; i<pizza.R; i++){
+        for(j=0; j<pizza.C; j++){
+            c = pizza.cells[i][j];
+            if(c == 'T')
+              count[0]++;
+            if(c == 'M'){
+              count[1]++;
+            }
+        }
+    }
+
+    return count;
+}
+
+int worst_fit(Pizza pizza, L, H){
+  char *start;
+  i=0
+  j=0
+  x = 1;
+  y = 1;
+
+  start = &pizza.cells[i][j];
+
+}
