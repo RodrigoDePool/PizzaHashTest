2538dce5080578601389ed8106ae6c78d518bd67
hzzhyj <hzzhyj1997@gmail.com>
2017-02-23 19:06:28 +0100

    alghksadlj

--- hash code/library.h
+++ hash code/library.h
@@ -5,32 +5,43 @@
 #define MAX_VIDS 10000
 #define MAX_REQUESTS 1000000
 
-typedef struct{
-	int size;
-	int id;
-	/*servers where it is put?*/
+typedef struct {
+    int V;
+    int E;
+    int R;
+    int C;
+    int X;
+    Endpoint[1000] endpoints;
+    Request[1000000] requests;
+    Video[10000] videos;
+} Database;
+
+typedef struct {
+    int size;
+    int id;
+    /*servers where it is put?*/
 }Video;
 
-typedef struct{
-	int id;
-	int lat_base;
-	int lat[MAX_VIDS];
-	Server cache[1000];
-	int num_cache;
+typedef struct {
+    int id;
+    int lat_base;
+    int lat[MAX_VIDS];
+    Server cache[1000];
+    int num_cache;
 }Endpoint;
 
-typedef struct{
-	int id;
-	int num_videos;
-	Video videos[MAX_VIDS];
+typedef struct {
+    int id;
+    int num_videos;
+    Video videos[MAX_VIDS];
 }Server;
 
-typedef struct{
-	int server_id;
-	int endpoint_id;
-	int number;
+typedef struct {
+    int server_id;
+    int endpoint_id;
+    int number;
 }Request;
 
-typedef struct{
-	Request request[MAX_REQUEST];
+typedef struct {
+    Request request[MAX_REQUEST];
 }Reqs;
--- hash code/read_file.h
+++ hash code/read_file.h
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "library.h"
 
 
 
-typedef struct {
-    int V;
-    int E;
-    int R;
-    int C;
-    int X;
-    Endpoint[1000] endpoints;
-    Request[1000000] requests;
-    Video[10000] videos;
-} Database;
 
 Database* read_file(char* path);
 
